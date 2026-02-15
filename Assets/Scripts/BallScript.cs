bd25da434bf260cbfde3ee1bf99c4bd53b254f48
DominicJakob <dominic.a.jakob@gmail.com>
2026-02-15 18:29:34 +0100

    new

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,47 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float maxInitialAngle = 1.67f;
+    public float moveSpeed = 2f;
+    public float startX = 0f;
+    public float maxstartY = 4f;
+
+    private void Start()
+    {
+        InitialPush();
+        
+    }
+
+    private void InitialPush()
+    {
+        Vector2 dir = Random.value < 0.5? Vector2.left : Vector2.right;
+        
+        
+        dir.y = Random.Range(-maxInitialAngle, maxInitialAngle);
+        rb2d.linearVelocity = dir * moveSpeed;
+    }
+
+    private void ResetBall()
+    {
+        float PosY = Random.Range(-maxstartY, maxstartY);
+        Vector2 position = new Vector2(startX, PosY);
+        transform.position = position;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Scorezone scorezone = collision.GetComponent<Scorezone>();
+        if (scorezone)
+        { 
+            ResetBall();
+            InitialPush();
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public int scorePlayer1, scorePlayer2;
+
+    public OnScoreZoneReached(int id)
+    {
+
+    }
+}
